1a7876433d7cf96942a86c517da1d6e0b564c8d1
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-20 17:39:36 +0300

    Update main

--- app/src/main/java/com/tufanakcay/storingdata/MainActivity.kt
+++ app/src/main/java/com/tufanakcay/storingdata/MainActivity.kt
@@ -1,18 +1,61 @@
 package com.tufanakcay.storingdata
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import com.tufanakcay.storingdata.databinding.ActivityMainBinding
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var sharedPreferences : SharedPreferences
+    var ageFromPreferences : Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //SharedPrefences Initialize
+        sharedPreferences = this.getSharedPreferences("com.tufanakcay.storingdata",
+            Context.MODE_PRIVATE)
+
+        ageFromPreferences = sharedPreferences.getInt("age",-1)
+
+        if (ageFromPreferences == -1) {
+            binding.textView.text = "Your Age: "
+        } else {
+            binding.textView.text = "Your Age: $ageFromPreferences"
+        }
 
 
     }
 
+    fun save(view : View){
+
+        val myAge = binding.editText.text.toString().toIntOrNull()
+
+        if (myAge != null) {
+            binding.textView.text = "Your Age: " + myAge
+            sharedPreferences.edit().putInt("age",myAge).apply()
+        }
 
 
+    }
+
+    fun delete(view: View) {
+
+        ageFromPreferences = sharedPreferences.getInt("age",-1)
+
+        if (ageFromPreferences != -1) {
+            sharedPreferences.edit().remove("age").apply()
+            binding.textView.text = "Your Age: "
+        }
+
+    }
 }
